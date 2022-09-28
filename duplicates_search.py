f98092cc5adf69be1021a745b5f2440f75a4af51
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2022-09-28 13:02:08 +0300

    Class DuplicateSearcher has been added

--- duplicates_search.py
+++ duplicates_search.py
@@ -1,10 +1,17 @@
 import os
 import time
 
+class DuplicatesSeacher:
+    def search(self, dir):
+        print('Start searching\n...')
+    
+
 if __name__ == '__main__':
-	time_start = time.perf_counter()
-	print('Hello!')
-	total_time = time.perf_counter() - time_start
-	print(
-		f'Search has finished in {round(total_time*1e3, 3)} ms'
-	)
+    time_start = time.perf_counter()
+    print('This is program for duplicates searching.')
+    total_time = time.perf_counter() - time_start
+    ds = DuplicatesSeacher()
+    ds.search('.')
+    print(
+        f'Search has finished in {round(total_time*1e3, 3)} ms'
+    )
